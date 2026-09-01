aff682a64b68e8c996624d9a53c1a635ca10184d
caio <caioafreitasbarbosa@gmail.com>
2026-09-01 16:56:02 -0300

    chore: mover a API para a porta 3100
    Evita conflito com outros projetos Next que usam a 3000.

--- apps/mobile/lib/api.ts
+++ apps/mobile/lib/api.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const API_BASE = __DEV__ ? "http://localhost:3000" : "https://sua-api.com";
+export const API_BASE = __DEV__ ? "http://localhost:3100" : "https://sua-api.com";
 
 export async function apiFetch(path: string, options: RequestInit = {}) {
   const token = await AsyncStorage.getItem("@cycla:token");
